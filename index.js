460cad1b66e4380d49d6df1e2c7805b404674c37
Mikhail Chernyshev <newmishach@gmail.com>
2022-10-04 11:15:34 +0300

    реализовал печать имени через js

---
+++ index.js
@@ -0,0 +1,26 @@
+const text = " Mikhail Chernyshev!";
+const title = document.querySelector('.wellcome__title_print')
+
+let line = 0;
+let count = 0;
+let result = '';
+
+function typeLine() {
+    let interval = setTimeout(
+    () => {
+    result += text[line][count]
+    title.innerHTML = result +'|';
+    count++;
+    if (count >= text[line].length) {
+        count = 0;
+        line++;
+if (line == text.length) {
+    clearTimeout(interval);
+    title.innerHTML =result;
+    return true;
+  }
+}
+typeLine();
+    }, 300)
+  }
+typeLine();
