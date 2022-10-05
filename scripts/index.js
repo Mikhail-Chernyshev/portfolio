a2da1e4e4ee04553eb837dde507585d3ee55dce0
Mikhail Chernyshev <newmishach@gmail.com>
2022-10-05 16:09:40 +0300

    реализовал основные функции, надо проверить всё

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,62 @@
-
+// var today = new Date();
+// var year = today.getFullYear();
+// const footerYear = document.querySelector('.footer__year');
+// footerYear.innerHTML = year;
 
 const text = " Mikhail Chernyshev!";
-const title = document.querySelector('.wellcome__title_print')
-
+const title = document.querySelector(".wellcome__title_print");
 let line = 0;
 let count = 0;
-let result = '';
-
+let result = "";
 function typeLine() {
-    let interval = setTimeout(
-    () => {
-    result += text[line][count]
-    title.innerHTML = result +'|';
+  let interval = setTimeout(() => {
+    result += text[line][count];
+    title.innerHTML = result + "|";
     count++;
     if (count >= text[line].length) {
-        count = 0;
-        line++;
-if (line == text.length) {
-    clearTimeout(interval);
-    title.innerHTML =result;
-    return true;
-  }
+      count = 0;
+      line++;
+      if (line == text.length) {
+        clearTimeout(interval);
+        title.innerHTML = result;
+        return true;
+      }
+    }
+    typeLine();
+  }, 300);
 }
 typeLine();
-    }, 300)
-  }
-typeLine();
-title.addEventListener('click', () => {
-  result = ' Михаил Чернышев!';
-  title.innerHTML = result;
-})
 
-elementsNight = document.querySelectorAll('.night')
-const page = document.querySelector('.page')
-const buttonTheme = document.querySelector('.header__change-theme')
+title.addEventListener("click", () => {
+  result = " Михаил Чернышев!"; 
+  let resultEn = " Mikhail Chernyshev!";
+  if (title.textContent === " Mikhail Chernyshev!") {
+    title.innerHTML = result;
+  } else { title.innerHTML = resultEn}
+});
+
+elementsNight = document.querySelectorAll(".night");
+const page = document.querySelector(".page");
+const buttonTheme = document.querySelector(".header__change-theme");
 function changeTheme() {
-  elementsNight.forEach(element => {
-    element.classList.toggle('page_theme_dark')
+  elementsNight.forEach((element) => {
+    element.classList.toggle("page_theme_dark");
   });
-  // page.classList.remove('.page')
-  // elementsNight.classList.toggle('page_theme_dark');
 }
-console.log(buttonTheme)
-buttonTheme.addEventListener('click', changeTheme)
+buttonTheme.addEventListener("click", () => {
+  changeTheme();
+  if (buttonTheme.textContent === 'N') {
+    buttonTheme.innerHTML = 'D';
+  } else {
+    buttonTheme.innerHTML = 'N';
+  }
+});
+
+const goTopBtn = document.querySelector('.footer__up');
+function backToTop() {
+  if (window.pageYOffset > 0) {
+    window.scrollBy(0, -80);
+    setTimeout(backToTop, 30);
+  }
+}
+goTopBtn.addEventListener('click', backToTop);
